d69d948bb8ff3a6765a1d0f803f91ca4b7046542
NicholasDomitio <nicholas.domitio@gmail.com>
2018-01-29 10:35:20 -0500

    initial commit

---
+++ IntrotoOOP/Cat.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntrotoOOP
+{
+    class Cat
+    {
+        //I need STATES
+        //I need BEHAVIORS
+    }
+}
